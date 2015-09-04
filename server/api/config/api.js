e430705dec0698cebc394da16c970e52f7479568
kandros <kandros5591@gmail.com>
2015-09-04 08:39:25 +0200

    Add connection method to api object

--- server/api/config/api.js
+++ server/api/config/api.js
@@ -1,3 +1,15 @@
 API  = {
-  
-}
+  connection: function ( request ) {
+    var getRequestContentes = API.utility.getRequestContents( request );
+    return  { data : getRequestContentes };
+  },
+  handleRequest: function( context, resource, method ) {
+    var connection = API.connection( context.request  );
+    if ( !connection.error  ) {
+      API.methods [ resource ][ methods ]( context , connection );
+    } else {
+      API.utility.response( context, 401, connection );
+    }
+  }
+
+};
