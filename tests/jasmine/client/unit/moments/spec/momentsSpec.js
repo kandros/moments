11486a5243571d4ea6c89696290cf5ada719d226
kandros <kandros5591@gmail.com>
2015-09-08 23:09:38 +0200

    tests

--- tests/jasmine/client/unit/moments/spec/momentsSpec.js
+++ tests/jasmine/client/unit/moments/spec/momentsSpec.js
@@ -1,20 +1,11 @@
-describe('True sd', function () {
-
-  it('should be truthy', function () {
-    var x = true;
-    expect(x).toBeTruthy();
-    expect(x).not.toBeFalsy();
-  });
-
-});
-
 describe('Moments collection', function () {
+
   it('should be truthy', function () {
     expect(Moments).toBeTruthy();
   });
 
   it('should not be null', function () {
-    expect(Moments).toBeTruthy();
+    expect(Moments).not.toBeNull();
   });
 
 });
