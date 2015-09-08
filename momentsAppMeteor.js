a45602d6288ceb728d8b8d855a16746894bc5071
kandros <kandros5591@gmail.com>
2015-09-08 11:56:30 +0200

    implement basic restful api

--- momentsAppMeteor.js
+++ momentsAppMeteor.js
@@ -150,4 +150,16 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  // Global Api configuration
+  var Api = new Restivus({
+    prettyJson: true
+  });
+
+  // Generates: GET, POST on /api/items and GET, PUT, DELETE on
+  // /api/items/:id for the Moments collection
+  Api.addCollection(Moments);
+
+
+
 }
