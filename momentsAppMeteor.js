304f8e8071de6ec08d2f06b20ed3725c04fa00a7
kandros <kandros5591@gmail.com>
2015-09-08 19:11:55 +0200

    move syncedCron to own file

--- momentsAppMeteor.js
+++ momentsAppMeteor.js
@@ -98,21 +98,3 @@ Meteor.methods({
     }
   }
 });
-
-if (Meteor.isServer) {
-
-}
-
-
-
-// SyncedCron.add({
-//   name: 'log the time every 5 min',
-//   schedule: function (parser) {
-//     return parser.text('every 5 mins');
-//   },
-//   job: function () {
-//     console.log(Date());
-//   }
-// });
-//
-// SyncedCron.start();
---
+++ server/syncedCron.js
@@ -0,0 +1,11 @@
+SyncedCron.add({
+  name: 'log the time every 5 min',
+  schedule: function (parser) {
+    return parser.text('every 5 mins');
+  },
+  job: function () {
+    console.log(Date());
+  }
+});
+
+SyncedCron.start();
